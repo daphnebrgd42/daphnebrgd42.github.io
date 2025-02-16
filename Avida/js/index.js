6960d5df1e773283809d6055e27d6390d96acdc9
Daphne <daphnechiu0223@gmail.com>
2025-02-16 18:53:39 +0800

    Avida_footer

--- Avida/js/index.js
+++ Avida/js/index.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".poster.swiper", {
+const city_swiper = new Swiper(".tw-top-cities.poster.swiper", {
   slidesPerView: "auto",
   freeMode: true,
   spaceBetween: 10,
@@ -7,12 +7,30 @@ const swiper = new Swiper(".poster.swiper", {
     prevEl: ".pre-btn",
   },
   breakpoints: {
-    
     992: {
       slidesPerView: 5,
-      slidesPerGroup: 2,
+      slidesPerGroup: 5,
       width: 1000,
       spaceBetween: 10,
     },
   },
 });
+
+const discount_swiper = new Swiper(".discounts.poster.swiper", {
+    slidesPerView:2,
+    freeMode: true,
+    spaceBetween: 3,
+    navigation: {
+      nextEl: ".next-btn",
+      prevEl: ".pre-btn",
+    },
+    breakpoints: {
+      992: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        width: 1000,
+        spaceBetween: 3,
+      },
+    },
+  });
+  
