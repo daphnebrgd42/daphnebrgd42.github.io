2e29aef7721d359130e4e7edd043d9d1b850acf6
Daphne <daphnechiu0223@gmail.com>
2025-02-18 05:33:39 +0800

    Avida_OK

--- Avida/js/index.js
+++ Avida/js/index.js
@@ -1,4 +1,4 @@
-const city_swiper = new Swiper(".tw-top-cities.poster.swiper", {
+const city_swiper = new Swiper(".tw-top-cities.swiper", {
   slidesPerView: "auto",
   freeMode: true,
   spaceBetween: 10,
@@ -16,21 +16,31 @@ const city_swiper = new Swiper(".tw-top-cities.poster.swiper", {
   },
 });
 
-const discount_swiper = new Swiper(".discounts.poster.swiper", {
-    slidesPerView:1,
-    freeMode: true,
-    width:350,
-    navigation: {
-      nextEl: ".next-btn",
-      prevEl: ".pre-btn",
+const discount_swiper = new Swiper(".discounts.swiper", {
+  slidesPerView: 1,
+  freeMode: true,
+  width: 350,
+  navigation: {
+    nextEl: ".next-btn",
+    prevEl: ".pre-btn",
+  },
+  breakpoints: {
+    992: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+      width: 1000,
+      spaceBetween: 3,
     },
-    breakpoints: {
-      992: {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        width: 1000,
-        spaceBetween: 3,
-      },
+  },
+});
+
+const recommendCarousel = new Swiper(".hotels.swiper", {
+  slidesPerView: 1,
+  width: 300,
+  breakpoints: {
+    992: {
+      slidesPerView: 4,
+      width: 1000,
     },
-  });
-  
+  },
+});
